c7e8b50d0fff6ad20276fb868dd173390ab52f4e
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-01-22 12:53:39 +0530

    Database created

--- Assignments/week-3/04-mongo-with-jwt-auth/db/index.js
+++ Assignments/week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,19 +1,30 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://shuklashreyansh0207:gFyxp0L0uz3tgoQ8@cluster0.vlkn4.mongodb.net/course_selling_jwt');
 
 // Define schemas
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username : String,
+    password : String,
+    purchaseCourse : [{
+        ref : "Course"
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title :String,
+    description : String,
+    price : Number,
+    imageLink : String
 });
 
 const Admin = mongoose.model('Admin', AdminSchema);
