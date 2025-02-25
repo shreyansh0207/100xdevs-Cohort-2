7a86d4eed55871d19909debbf7cd0dc4ac818f3f
shreyansh0207 <shuklashreyansh0207@gmail.com>
2025-02-25 23:26:36 +0530

    Learnig about fetching the data

--- Daily-Progress/next-app-fetching/app/page.tsx
+++ Daily-Progress/next-app-fetching/app/page.tsx
@@ -16,7 +16,7 @@ export default async function Home() {
   return (
     <div className="flex flex-col justify-center h-screen">
     <div className="flex justify-center">
-        <div className="border p-8 rounded bg-center">
+        <div className="border p-8 rounded">
             <div>
                 Name:{userDetails?.name}
             </div>
